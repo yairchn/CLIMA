d101ee7c74dfd6c1c6a33e83942355d21e90cb68
Charles Kawczynski <kawczynski.charles@gmail.com>
2020-03-04 08:27:40 -0800

    Improve surface fluxes

--- test/Atmos/Parameterizations/SurfaceFluxes/runtests.jl
+++ test/Atmos/Parameterizations/SurfaceFluxes/runtests.jl
@@ -1,8 +1,6 @@
 using Test
 
 using CLIMA.SurfaceFluxes
-using CLIMA.SurfaceFluxes.Nishizawa2018
-using CLIMA.SurfaceFluxes.Byun1990
 using CLIMA.MoistThermodynamics
 using CLIMA.RootSolvers
 
@@ -12,69 +10,34 @@ using CLIMA.RootSolvers
 
 @testset "SurfaceFluxes" begin
 
-  shf, lhf, T_b, qt_b, ql_b, qi_b, alpha0_0 = 60.0, 50.0, 350.0, 0.01, 0.002, 0.0001, 1.0
-  buoyancy_flux = SurfaceFluxes.compute_buoyancy_flux(shf,
-                                                 lhf,
-                                                 T_b,
-                                                 qt_b,
-                                                 ql_b,
-                                                 qi_b,
-                                                 alpha0_0
-                                                 )
-  @test buoyancy_flux ≈ 0.0017808608107074118
-end
+  x_initial = [100, 15.0, 350.0]
+  F_exchange = [2.0, 3.0]
+  z_0 = [1.0, 1.0]
+  dimensionless_number = [1.0, 0.74]
+  x_ave = [5.0, 350.0]
+  x_s   = [0.0, 300.0]
 
-@testset "SurfaceFluxes.Byun1990" begin
-  u, flux = 0.1, 0.2
-  MO_len = Byun1990.compute_MO_len(u, flux)
-  @test MO_len ≈ -0.0125
+  Δz = 2.0
+  z = 0.5
+  θ_bar = 300.0
+  a = 4.7
+  pottemp_flux_given = -200.0
+  args = x_initial, x_ave, x_s, z_0, F_exchange, dimensionless_number, θ_bar, Δz, z, a, pottemp_flux_given
+  sfc = surface_conditions(args[1:end-1]...)
 
-  u_ave, buoyancy_flux, z_0, z_1 = 0.1, 0.2, 2.0, 5.0
-  γ_m, β_m = 15.0, 4.8
-  tol_abs, iter_max = 1e-3, 10
-  u_star = Byun1990.compute_friction_velocity(u_ave,
-                                              buoyancy_flux,
-                                              z_0,
-                                              z_1,
-                                              β_m,
-                                              γ_m,
-                                              tol_abs,
-                                              iter_max
-                                              )
-  @test u_star ≈ 0.201347256193615 atol=tol_abs
+  @test sfc.L_MO                   ≈ 54.563405359719404
+  @test sfc.pottemp_flux_star      ≈ -1132.9097989525164
+  @test all(sfc.flux              .≈ [-86.79000158448329, -1132.9097989138224])
+  @test all(sfc.x_star            .≈ [9.316115155175106, 121.60753490520031])
+  @test all(sfc.K_exchange        .≈ [0.9969164175880834, 0.08477271454000379])
 
+  sfc = surface_conditions(args...)
 
-  Ri, z_b, z_0, Pr_0 = 10, 2.0, 5.0, 0.74
-  γ_m, γ_h, β_m, β_h = 15.0, 9.0, 4.8, 7.8
-  cm, ch, L_mo = Byun1990.compute_exchange_coefficients(Ri, z_b, z_0, γ_m, γ_h, β_m, β_h, Pr_0)
-  @test cm ≈ 19.700348427787368
-  @test ch ≈ 3.3362564728997803
-  @test L_mo ≈ -14.308268023583906
-
-  Ri, z_b, z_0, Pr_0 = -10, 10.0, 1.0, 0.74
-  γ_m, γ_h, β_m, β_h = 15.0, 9.0, 4.8, 7.8
-  cm, ch, L_mo = Byun1990.compute_exchange_coefficients(Ri, z_b, z_0, γ_m, γ_h, β_m, β_h, Pr_0)
-  @test cm ≈ 0.33300280321092746
-  @test ch ≈ 1.131830939627489
-  @test L_mo ≈ -0.3726237964444814
+  @test sfc.L_MO                   ≈ 405.8862509767147
+  @test sfc.pottemp_flux_star      ≈ -199.99999999999997
+  @test all(sfc.flux              .≈ [-104.07858678549196, -1399.2078659154145])
+  @test all(sfc.x_star            .≈ [10.20189133374258, 137.15181039887756])
+  @test all(sfc.K_exchange        .≈ [6.771981702484999, 0.5591365770421184])
 
 end
 
-@testset "SurfaceFluxes.Nishizawa2018" begin
-  u, θ, flux = 2, 350, 20
-  MO_len = Nishizawa2018.compute_MO_len(u, θ, flux)
-  @test MO_len ≈ -35.67787971457696
-
-  u_ave, θ, flux, Δz, z_0, a = 110, 350, 20, 100, 0.01, 5
-  Ψ_m_tol, tol_abs, iter_max = 1e-3, 1e-3, 10
-  u_star = Nishizawa2018.compute_friction_velocity(u_ave, θ, flux, Δz, z_0, a, Ψ_m_tol, tol_abs, iter_max)
-  @test u_star ≈ 5.526644550864822 atol=tol_abs
-
-  z, F_m, F_h, a, u_star, θ, flux, Pr = 1.0, 2.0, 3.0, 5, 110, 350, 20, 0.74
-
-  K_m, K_h, L_mo = Nishizawa2018.compute_exchange_coefficients(z,F_m,F_h,a,u_star,θ,flux,Pr)
-  @test K_m ≈ -11512.071612359368
-  @test K_h ≈ -6111.6196776263805
-  @test L_mo ≈ -5.935907237512742e6
-
-end
