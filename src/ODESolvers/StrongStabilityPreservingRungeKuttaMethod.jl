c8c8ac3741745ded41c8ad184e34983896deb6e6
Lucas C Wilcox <lucas@swirlee.com>
2020-03-05 21:37:45 -0800

    Sync kernels with events in the default queue
    This removes the draconian syncing we had before and replaces it with
    syncing to events in the default queue/stream.  This should mimic what
    was in place before when all kernels were launched in the default
    queue/stream.

--- src/ODESolvers/StrongStabilityPreservingRungeKuttaMethod.jl
+++ src/ODESolvers/StrongStabilityPreservingRungeKuttaMethod.jl
@@ -118,11 +118,11 @@ function dostep!(Q, ssp::StrongStabilityPreservingRungeKutta, p,
     if s == length(RKB)
       slow_scaling = in_slow_scaling
     end
-    sync_device(device(Q))
+    event = Event(device(Q))
     event = update!(device(Q), groupsize)(
       rv_Rstage, rv_Q, rv_Qstage, RKA[s,1], RKA[s,2], RKB[s], dt,
       slow_δ, slow_rv_dQ, slow_scaling; ndrange=length(rv_Q))
-    wait(event)
+    wait(device(Q), event)
   end
   rv_Q .= rv_Qstage
 end
