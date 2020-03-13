de7383b984a46daf27ada3eb2f176aad29e95ccd
Charles Kawczynski <kawczynski.charles@gmail.com>
2020-03-12 20:06:14 -0700

    Clean up Courant computations

--- src/Atmos/Model/courant.jl
+++ src/Atmos/Model/courant.jl
@@ -1,9 +1,23 @@
+using ..Grids: Direction, HorizontalDirection, VerticalDirection
+
 advective_courant(m::AtmosLinearModel, a...) = advective_courant(m.atmos, a...)
 
 nondiffusive_courant(m::AtmosLinearModel, a...) =
     nondiffusive_courant(m.atmos, a...)
 
 diffusive_courant(m::AtmosLinearModel, a...) = diffusive_courant(m.atmos, a...)
+
+norm_u(state::Vars, k̂::AbstractVector, ::VerticalDirection) =
+    abs(dot(state.ρu, k̂)) / state.ρ
+norm_u(state::Vars, k̂::AbstractVector, ::HorizontalDirection) =
+    norm((state.ρu .- dot(state.ρu, k̂) * k̂) / state.ρ)
+norm_u(state::Vars, k̂::AbstractVector, ::Direction) = norm(state.ρu / state.ρ)
+
+norm_ν(ν::Real, k̂::AbstractVector, ::Direction) = ν
+norm_ν(ν::AbstractVector, k̂::AbstractVector, ::VerticalDirection) = dot(ν, k̂)
+norm_ν(ν::AbstractVector, k̂::AbstractVector, ::HorizontalDirection) =
+    norm(ν - dot(ν, k̂) * k̂)
+norm_ν(ν::AbstractVector, k̂::AbstractVector, ::Direction) = norm(ν)
 
 function advective_courant(
     m::AtmosModel,
@@ -15,14 +29,8 @@ function advective_courant(
     direction,
 )
     k̂ = vertical_unit_vector(m.orientation, aux)
-    if direction isa VerticalDirection
-        norm_u = abs(dot(state.ρu, k̂)) / state.ρ
-    elseif direction isa HorizontalDirection
-        norm_u = norm((state.ρu - dot(state.ρu, k̂) * k̂) / state.ρ)
-    else
-        norm_u = norm(state.ρu / state.ρ)
-    end
-    return Δt * norm_u / Δx
+    normu = norm_u(state, k̂, direction)
+    return Δt * normu / Δx
 end
 
 function nondiffusive_courant(
@@ -35,14 +43,8 @@ function nondiffusive_courant(
     direction,
 )
     k̂ = vertical_unit_vector(m.orientation, aux)
-    if direction isa VerticalDirection
-        norm_u = abs(dot(state.ρu, k̂)) / state.ρ
-    elseif direction isa HorizontalDirection
-        norm_u = norm((state.ρu - dot(state.ρu, k̂) * k̂) / state.ρ)
-    else
-        norm_u = norm(state.ρu / state.ρ)
-    end
-    return Δt * (norm_u + soundspeed(m, m.moisture, state, aux)) / Δx
+    normu = norm_u(state, k̂, direction)
+    return Δt * (normu + soundspeed(m, m.moisture, state, aux)) / Δx
 end
 
 function diffusive_courant(
@@ -55,21 +57,7 @@ function diffusive_courant(
     direction,
 )
     ν, τ = turbulence_tensors(m.turbulence, state, diffusive, aux, 0)
-    FT = eltype(state)
-
-    if ν isa Real
-        return Δt * ν / (Δx * Δx)
-    else
-        k̂ = vertical_unit_vector(m.orientation, aux)
-        ν_vert = dot(ν, k̂)
-
-        if direction isa VerticalDirection
-            return Δt * ν_vert / (Δx * Δx)
-        elseif direction isa HorizontalDirection
-            ν_horz = ν - ν_vert * k̂
-            return Δt * norm(ν_horz) / (Δx * Δx)
-        else
-            return Δt * norm(ν) / (Δx * Δx)
-        end
-    end
+    k̂ = vertical_unit_vector(m.orientation, aux)
+    normν = norm_ν(ν, k̂, direction)
+    return Δt * normν / (Δx * Δx)
 end
